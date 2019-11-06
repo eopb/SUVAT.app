b021cce142cc2ac0874c1dd4d2b712336d7feedc
Ethan Brierley <ethanboxx@gmail.com>
2019-11-06 15:26:29 +0000

    solution

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,31 @@
       href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap"
       rel="stylesheet"
     />
-    <LetterBox @letter-value-changed="bruh" letter="s" />
-    <LetterBox @letter-value-changed="bruh" letter="u" />
-    <LetterBox @letter-value-changed="bruh" letter="v" />
-    <LetterBox @letter-value-changed="bruh" letter="a" />
-    <LetterBox @letter-value-changed="bruh" letter="t" />
-    <h1>{{ suvat.s }}</h1>
+    <div id="grid-wrap">
+      <LetterBox @letter-value-changed="bruh" letter="s" />
+      <Solution />
+      <LetterBox @letter-value-changed="bruh" letter="u" />
+      <Solution />
+      <LetterBox @letter-value-changed="bruh" letter="v" />
+      <Solution />
+      <LetterBox @letter-value-changed="bruh" letter="a" />
+      <Solution />
+      <LetterBox @letter-value-changed="bruh" letter="t" />
+      <Solution />
+    </div>
+    <!-- <h1>{{ suvat.s + 5}}</h1> -->
   </div>
 </template>
 
 <script>
 import LetterBox from "./components/LetterBox.vue";
+import Solution from "./components/Solution.vue";
 
 export default {
   name: "app",
   components: {
-    LetterBox
+    LetterBox,
+    Solution
   },
   data: () => ({
     creditLimit: "",
@@ -61,13 +70,24 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Roboto, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+body 
+  margin: 0
+  *,
+  *::after,
+  *::before
+    box-sizing: inherit
+
+#app 
+  font-family: Roboto, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
+#grid-wrap 
+  display: grid
+  grid-template-columns: 300px 1fr
+  grid-template-rows: repeat(5, 1fr)
+  height: 100vh
 </style>
---
+++ src/components/Solution.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="solution">
+    <h1>foo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Solution",
+  props: ["suvat"],
+  data: () => ({
+    creditLimit: ""
+  })
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#solution {
+  background-color: green;
+}
+h1 {
+  margin: 0;
+}
+</style>
