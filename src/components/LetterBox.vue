5a84d5c70f5c4dba2cb555aef9d3e5df60a3a60a
Ethan Brierley <ethanboxx@gmail.com>
2019-11-06 10:03:24 +0000

    Does not work

--- src/components/LetterBox.vue
+++ src/components/LetterBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="letter-box">
     <h1>{{ letter }}</h1>
-    <input type="number" step="any" />
+    <input v-model="letterValue" type="number" step="any" />
   </div>
 </template>
 
@@ -11,7 +11,12 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class LetterBox extends Vue {
   @Prop() private letter!: string;
-}
+  methods: {
+    valueChange: () =>{
+      console.log(this.letterValue);
+    };
+  }
+}\
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
