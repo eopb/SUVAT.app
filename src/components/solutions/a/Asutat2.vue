824ed84bceb90e8c85c62369814373f838d48d7f
Ethan Brierley <ethanboxx@gmail.com>
2019-11-09 11:42:47 +0000

    use mixins with one file

--- src/components/solutions/a/Asutat2.vue
+++ src/components/solutions/a/Asutat2.vue
@@ -6,95 +6,85 @@ div
     sub
       | ms
       sup -2
-  transition(appear, name='fade')
-    h2 Solution
-  transition(appear, name='fade')
-    h3 Using the equation.
-  transition(appear, name='fade')
-    div
-      math
-        mi s
-        mo =
-        mi u
+  +th2
+    | Solution
+  +th3
+    | Using the equation.
+  +tmath
+    mi s
+    mo =
+    mi u
+    mi t
+    mo +
+    mfrac
+      mn 1
+      mn 2
+    mi a
+    msup
+      mi t
+      mn 2
+  +th3
+    | First lets rearrange this equation by subtracting
+    math
+      mi u
+      mi t
+    | from both sides.
+  +tmath
+    mi s
+    mo -
+    mi u
+    mi t
+    mo =
+    mfrac
+      mn 1
+      mn 2
+    mi a
+    msup
+      mi t
+      mn 2
+  +th3
+    | First lets rearrange this equation by dividing both sides by
+    math
+      mfrac
+        mn 1
+        mn 2
+      msup
         mi t
-        mo +
-        mfrac
-          mn 1
-          mn 2
-        mi a
-        msup
-          mi t
-          mn 2
-  transition(appear, name='fade')
-    h3
-      | First lets rearrange this equation by subtracting
-      math
-        mi u
-        mi t
-      |         from both sides.
-  transition(appear, name='fade')
-    div
-      math
+        mn 2
+  +tmath
+    mfrac
+      div
         mi s
         mo -
         mi u
         mi t
-        mo =
-        mfrac
-          mn 1
-          mn 2
-        mi a
-        msup
-          mi t
-          mn 2
-  transition(appear, name='fade')
-    h3
-      | First lets rearrange this equation by dividing both sides by
-      math
+      mpadded
         mfrac
           mn 1
           mn 2
         msup
           mi t
           mn 2
-  transition(appear, name='fade')
-    div
-      math
+    mo =
+    mi a
+  +th3
+    | Lastly enter known values.
+  +tmath
+    mfrac
+      div
+        mn {{ suvat.s }}
+        mo -
+        mn {{ suvat.u }}
+        mo &times;
+        mn {{ suvat.t }}
+      mpadded
         mfrac
-          div
-            mi s
-            mo -
-            mi u
-            mi t
-          mpadded
-            mfrac
-              mn 1
-              mn 2
-            msup
-              mi t
-              mn 2
-        mo =
-        mi a
-  transition(appear, name='fade')
-    h3 Lastly enter known values.
-  transition(appear, name='fade')
-    div
-      math
-        mfrac
-          div
-            mn {{ suvat.s }}
-            mo -
-            mn {{ suvat.u }}
-            mo &times;
-            mn {{ suvat.t }}
-          mpadded
-            mfrac
-              mn 1
-              mn 2
-            mo &times;
-            msup
-              mn {{ suvat.t }}
-              mn 2
+          mn 1
+          mn 2
+        mo &times;
+        msup
+          mn {{ suvat.t }}
+          mn 2
 </template>
 
 <script>
