6eb9db43a961865ce2ed3d2f427606d26b8310d3
Ethan Brierley <ethanboxx@gmail.com>
2019-11-15 18:05:29 +0000

    links

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,11 +3,28 @@
     <div>
       <h1>About suvat</h1>
       <p>Suvat is first and foremost acronym.</p>
-      <p>s = displacement</p>
-      <p>u = initial velocity</p>
-      <p>v = final velocity</p>
-      <p>a = acceleration</p>
-      <p>t = time</p>
+      <p>
+        s =
+        <a href="https://en.wikipedia.org/wiki/Displacement_(vector)"
+          >displacement</a
+        >
+      </p>
+      <p>
+        u = initial
+        <a href="https://en.wikipedia.org/wiki/Velocity">velocity</a>
+      </p>
+      <p>
+        v = final
+        <a href="https://en.wikipedia.org/wiki/Velocity">velocity</a>
+      </p>
+      <p>
+        a =
+        <a href="https://en.wikipedia.org/wiki/Acceleration">acceleration</a>
+      </p>
+      <p>
+        t =
+        <a href="https://en.wikipedia.org/wiki/Time">time</a>
+      </p>
       <p>These variables are linked by the suvat equations.</p>
       <Mathr
         formula="$v=u+at\\s=ut+\frac{1}{2}at^2\\s=\frac{1}{2}(u+v)t\\v^2=u^2+2as\\s=vt-\frac{1}{2}at^2$"
@@ -15,6 +32,13 @@
       <p>
         Using these equations any three variables combined with the correct
         equations can be used to calcuate the other two values.
+      </p>
+      <p>
+        This makes it easy to solve physics problems with
+        <a
+          href="https://en.wikipedia.org/wiki/Equations_of_motion#Uniform_acceleration"
+          >constant acceleration</a
+        >.
       </p>
     </div>
   </div>
