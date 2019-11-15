212d8fdc90a746d87c11644c3539cd0fbce6477c
Ethan Brierley <ethanboxx@gmail.com>
2019-11-15 17:53:26 +0000

    a little more about

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,13 +2,28 @@
   <div id="about">
     <div>
       <h1>About suvat</h1>
+      <p>Suvat is first and foremost acronym.</p>
+      <p>s = displacement</p>
+      <p>u = initial velocity</p>
+      <p>v = final velocity</p>
+      <p>a = acceleration</p>
+      <p>t = time</p>
+      <p>These variables are linked by the suvat equations.</p>
+      <Mathr
+        formula="$v=u+at\\s=ut+\frac{1}{2}at^2\\s=\frac{1}{2}(u+v)t\\v^2=u^2+2as\\s=vt-\frac{1}{2}at^2$"
+      />
+      <p>Using these equations any three variables combined with the correct equations can be used to calcuate the other two values.</p>
     </div>
   </div>
 </template>
 
 <script>
+import Mathr from "./Mathr.vue";
 export default {
-  name: "About"
+  name: "About",
+  components: {
+    Mathr
+  }
 };
 </script>
 
@@ -22,4 +37,6 @@ export default {
     width: 90%
     @media (min-width: 820px)
       width: 800px
+    p
+      font-size: 1.1em
 </style>
